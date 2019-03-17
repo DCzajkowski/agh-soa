3270c52fe1de664eb8653033fbedb358193d663d
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-03-17 18:14:23 +0100

    Switched to BCrypt

--- lab2/guest-book/src/App/Domain/AuthService.java
+++ lab2/guest-book/src/App/Domain/AuthService.java
@@ -1,35 +1,27 @@
 package App.Domain;
 
+import App.Helpers.BCrypt;
+
 import javax.enterprise.context.ApplicationScoped;
-import javax.xml.bind.DatatypeConverter;
 import java.io.Serializable;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 
 @ApplicationScoped
 public class AuthService implements Serializable {
     private final List<User> users;
-    private final MessageDigest hashManager;
 
     public AuthService() {
         users = Arrays.asList(
-            new User("admin", "5f4dcc3b5aa765d61d8327deb882cf99", "", "")
+            new User("admin", "$2a$10$awMVKp0eI5.NC8efP4ppUuGb1ffG4k5VJJoyCY27yH/Pe0bu7zVtK", "", "")
         );
-
-        try {
-            hashManager = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("No md5 bro.", e);
-        }
     }
 
     public boolean attemptLogin(String username, String password) {
         return users.stream()
             .filter(_user -> _user.getUsername().equals(username))
             .findFirst()
-            .map(_user -> _user.getPassword().equals(DatatypeConverter.printHexBinary(hashManager.digest(password.getBytes())).toLowerCase()))
+            .map(_user -> BCrypt.checkpw(password, _user.getPassword()))
             .orElse(false);
     }
 }
