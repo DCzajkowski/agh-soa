f53cb8733eace07230afcfe5efd5498814969d19
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-03-23 01:36:32 +0100

    Added a neat annotation

--- lab3/survey/src/main/java/com/dczajkowski/helpers/Helpers.java
+++ lab3/survey/src/main/java/com/dczajkowski/helpers/Helpers.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class Helpers {
+    @SafeVarargs
     public static <T> T tap(T value, Consumer<T> ...callbacks) {
         Stream.of(callbacks).forEach(callback -> callback.accept(value));
 
