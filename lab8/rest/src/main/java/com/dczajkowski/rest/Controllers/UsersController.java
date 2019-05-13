da3f12ecd39b0cc2eb1a0e5fc82957500b6572a7
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-13 23:24:06 +0200

    Made crud actions for users

--- lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/UsersController.java
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/UsersController.java
@@ -1,15 +1,16 @@
 package com.dczajkowski.rest.Controllers;
 
+import com.dczajkowski.rest.Models.User;
 import com.dczajkowski.rest.Repositories.UsersRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/users")
+@Path("users")
 public class UsersController {
     @Inject
     private UsersRepository usersRepository;
@@ -17,8 +18,64 @@ public class UsersController {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public String index() throws JsonProcessingException {
         return objectMapper.writeValueAsString(usersRepository.get());
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@PathParam("id") int id) {
+        User user = usersRepository.find(id);
+
+        if (user == null) {
+            return Response.status(404).build();
+        }
+
+        return Response.ok(user).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User store(User user) {
+        usersRepository.create(user);
+
+        return user;
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") int id, User user_) {
+        User user = usersRepository.find(id);
+
+        if (user == null) {
+            return Response.status(422).build();
+        }
+
+        user.setName((user_.getName() != null) ? user_.getName() : user.getName());
+        user.setAge((user_.getAge() != 0) ? user_.getAge() : user.getAge());
+        user.setAvatar((user_.getAvatar() != null) ? user_.getAvatar() : user.getAvatar());
+
+        usersRepository.update(user);
+
+        return Response.ok(user).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response destroy(@PathParam("id") int id) {
+        boolean successful = usersRepository.remove(id);
+
+        if (!successful) {
+            return Response.status(422).build();
+        }
+
+        return Response.noContent().build();
     }
 }
--- lab8/rest/src/main/java/com/dczajkowski/rest/Repositories/UsersRepository.java
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Repositories/UsersRepository.java
@@ -10,4 +10,28 @@ public class UsersRepository extends Repository {
     public List<User> get() {
         return em.createQuery("SELECT u FROM User u LEFT JOIN u.favouriteMovies fm").getResultList();
     }
+
+    public void create(User user) {
+        update(user);
+    }
+
+    public void update(User user) {
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+        em.clear();
+    }
+
+    public boolean remove(int user_id) {
+        em.getTransaction().begin();
+        int status = em.createQuery("DELETE FROM User u where u.id = :id").setParameter("id", user_id).executeUpdate();
+        em.getTransaction().commit();
+        em.clear();
+
+        return status == 1;
+    }
+
+    public User find(int id) {
+        return em.find(User.class, id);
+    }
 }
