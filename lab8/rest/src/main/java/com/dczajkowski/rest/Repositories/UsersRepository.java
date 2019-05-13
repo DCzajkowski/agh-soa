f59eda4fbaec5eaec341ddaeddeb97fd7d4ba0c5
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-13 23:01:46 +0200

    Fixed the inner join

--- lab8/rest/src/main/java/com/dczajkowski/rest/Repositories/UsersRepository.java
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Repositories/UsersRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 @ApplicationScoped
 public class UsersRepository extends Repository {
     public List<User> get() {
-        return em.createQuery("SELECT u FROM User u INNER JOIN u.favouriteMovies fm").getResultList();
+        return em.createQuery("SELECT u FROM User u LEFT JOIN u.favouriteMovies fm").getResultList();
     }
 }
