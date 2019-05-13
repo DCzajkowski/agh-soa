5f161a7f347b6b8c34b4d173070c1e3328ad33df
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-13 23:31:46 +0200

    Added movies CRUD actions

---
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/MoviesController.java
@@ -0,0 +1,80 @@
+package com.dczajkowski.rest.Controllers;
+
+import com.dczajkowski.rest.Models.Movie;
+import com.dczajkowski.rest.Repositories.MoviesRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("movies")
+public class MoviesController {
+    @Inject
+    private MoviesRepository moviesRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String index() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(moviesRepository.get());
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@PathParam("id") int id) {
+        Movie movie = moviesRepository.find(id);
+
+        if (movie == null) {
+            return Response.status(404).build();
+        }
+
+        return Response.ok(movie).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Movie store(Movie movie) {
+        moviesRepository.create(movie);
+
+        return movie;
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") int id, Movie movie_) {
+        Movie movie = moviesRepository.find(id);
+
+        if (movie == null) {
+            return Response.status(422).build();
+        }
+
+        movie.setTitle((movie_.getTitle() != null) ? movie_.getTitle() : movie.getTitle());
+        movie.setUrl((movie_.getUrl() != null) ? movie_.getUrl() : movie.getUrl());
+
+        moviesRepository.update(movie);
+
+        return Response.ok(movie).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response destroy(@PathParam("id") int id) {
+        boolean successful = moviesRepository.remove(id);
+
+        if (!successful) {
+            return Response.status(422).build();
+        }
+
+        return Response.noContent().build();
+    }
+}
---
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Repositories/MoviesRepository.java
@@ -0,0 +1,37 @@
+package com.dczajkowski.rest.Repositories;
+
+import com.dczajkowski.rest.Models.Movie;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class MoviesRepository extends Repository {
+    public List<Movie> get() {
+        return em.createQuery("SELECT m FROM Movie m").getResultList();
+    }
+
+    public void create(Movie movie) {
+        update(movie);
+    }
+
+    public void update(Movie movie) {
+        em.getTransaction().begin();
+        em.persist(movie);
+        em.getTransaction().commit();
+        em.clear();
+    }
+
+    public boolean remove(int id) {
+        em.getTransaction().begin();
+        int status = em.createQuery("DELETE FROM Movie u where u.id = :id").setParameter("id", id).executeUpdate();
+        em.getTransaction().commit();
+        em.clear();
+
+        return status == 1;
+    }
+
+    public Movie find(int id) {
+        return em.find(Movie.class, id);
+    }
+}
