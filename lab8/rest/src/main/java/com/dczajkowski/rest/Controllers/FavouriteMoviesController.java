ad453df6de466bd16015a4dd35ea3699ac4d5bde
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-15 13:16:44 +0200

    Added a possibility for removing a favourite

--- lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/FavouriteMoviesController.java
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/FavouriteMoviesController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("users")
 public class FavouriteMoviesController {
@@ -40,6 +41,32 @@ public class FavouriteMoviesController {
         favouriteMovies.add(movie);
 
         user.setFavouriteMovies(favouriteMovies);
+
+        usersRepository.update(user);
+
+        return Response.status(201).entity(user).build();
+    }
+
+    @DELETE
+    @Path("/{user_id}/favourites/movies/{movie_id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response unfavourite(
+        @PathParam("user_id") int userId,
+        @PathParam("movie_id") int movieId
+    ) {
+        User user = usersRepository.find(userId);
+
+        if (user == null) {
+            return Response.status(404).build();
+        }
+
+        user.setFavouriteMovies(
+            new ArrayList<>(user.getFavouriteMovies())
+                .stream()
+                .filter(movie -> movie.getId() != movieId)
+                .collect(Collectors.toList())
+        );
 
         usersRepository.update(user);
 
