568c46cdd05e35b148a5c232e6279611ce55a3bc
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-15 13:10:17 +0200

    Added a  controller for the favourites movies

---
+++ lab8/rest/src/main/java/com/dczajkowski/rest/Controllers/FavouriteMoviesController.java
@@ -0,0 +1,48 @@
+package com.dczajkowski.rest.Controllers;
+
+import com.dczajkowski.rest.Models.Movie;
+import com.dczajkowski.rest.Models.User;
+import com.dczajkowski.rest.Repositories.MoviesRepository;
+import com.dczajkowski.rest.Repositories.UsersRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("users")
+public class FavouriteMoviesController {
+    @Inject
+    private UsersRepository usersRepository;
+
+    @Inject
+    private MoviesRepository moviesRepository;
+
+    @POST
+    @Path("/{user_id}/favourites/movies/{movie_id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response favourite(
+        @PathParam("user_id") int userId,
+        @PathParam("movie_id") int movieId
+    ) {
+        User user = usersRepository.find(userId);
+        Movie movie = moviesRepository.find(movieId);
+
+        if (user == null || movie == null) {
+            return Response.status(404).build();
+        }
+
+        List<Movie> favouriteMovies = new ArrayList<>(user.getFavouriteMovies());
+        favouriteMovies.add(movie);
+
+        user.setFavouriteMovies(favouriteMovies);
+
+        usersRepository.update(user);
+
+        return Response.status(201).entity(user).build();
+    }
+}
